032ec0bf9840512f76bb610ec850798e7199f412
Rene90 <rob_rene199027@hotmail.com>
2022-12-06 17:49:15 -0600

    Objetos literales final

--- 04Objetos/main.js
+++ 04Objetos/main.js
@@ -78,3 +78,84 @@ var objeto1 = new miClase("PERICO","VACA")
 console.log(objeto1)
 console.log(objeto1.propiedad1)//PERICO
 console.log(objeto1.miMetodo1())
+console.log(objeto1.propiedad2) //VACA
+console.log(objeto1.miMetodo2())
+
+class estudiantesDevf {
+    //propiedad privada se define con un #
+    #apellido =""
+
+    //propoiedades publicas facil de acceder a ellas
+    constructor(nombre,edad,ocupacion,soltero,animefavorito){
+        this.nombre = nombre
+        this.edad = edad
+        this.ocupacion= ocupacion
+        this.soltero = soltero
+        this.anime = animefavorito
+    }
+    get apellido(){
+        return this.#apellido
+    }
+    set apellido(nuevoApellido){
+        this.#apellido =nuevoApellido
+    }
+    //funciones get para obtener  los valores de mis propiedades
+    getPropiedades(){
+        return [this.nombre,this.edad,this.ocupacion,this.soltero,this.anime]
+    }
+    //Funciones set para cambiar los valores de mis propiedades
+    setPropiedades(nuevonombre,nuevaedad,nuevaocupacion,nuevosoltero,nuevoanime){
+        this.nombre = nuevonombre
+        this.edad = nuevaedad
+        this.ocupacion= nuevaocupacion
+        this.soltero = nuevosoltero
+        this.anime = nuevoanime
+    }
+    verTele(){
+        return `Soy ${this.nombre} y mi ocupacion es ${this.ocupacion}, pero ahorita estoy viendo mi anime favorito que es ${this.anime}`
+    }
+    cita(){
+        if(this.soltero){
+            return `Tengo ${this.edad} y estoy soltero`
+        }else{
+            return "SIII TENGO NOVI@"
+        }
+    }
+    //A veces los getters y setters son individuales
+    setNombre(nuevoNombre){
+        this.name = nuevoNombre
+    }
+}
+var luis = new estudiantesDevf("Luis",17,"estudiante",true,"chainsaw-man")//Esto no es un objeto literal , es un objeto que proviene de la clase estudiatesDevf
+var alejandro = new estudiantesDevf("Alejandro",19,"estudiante",true,"record of ragnarok")
+console.log(luis)
+console.log(luis.getPropiedades())//manera mas eficiente de traer las propiedades (get)
+console.log(luis.name,luis.edad)
+//Cambiar los parametros de mi objeto usando setters
+luis.setPropiedades("Luis",19,"estudiante",true,"onePunch man")
+console.log(luis.getPropiedades())
+luis.anime = "BORUTO"
+console.log(luis.getPropiedades())
+luis.apellido = "Jahuira"
+console.log(luis.apellido)
+//HERENCIA
+//Se heredan las propiedades y  metodos de la clase padre estudiantesDevf a la clase hija estudiantesDevfH
+//La herencia nos permite ahorrar codigo cuando queremos que una clase se derive de otra
+class estudiantesDevfH extends estudiantesDevf{
+    constructor(nombre,edad,ocupacion,soltero,anime,equipoSoccer){
+
+        super(nombre,edad,ocupacion,soltero,anime)
+        this.equipo = equipoSoccer
+    }
+    irAlEstadio(){
+        return `Me llamo ${this.nombre} Ire al estadio a ver a mi equipo favorito que es ${this.equipo}`
+    }
+
+}
+
+var Andres = new estudiantesDevfH("Andres",18,"estudiante",true,"Naruto","Barcelona")
+console.log(Andres.nombre)
+console.log(Andres.edad)
+console.log(Andres.verTele())
+console.log(luis.verTele())
+console.log(Andres.irAlEstadio())
